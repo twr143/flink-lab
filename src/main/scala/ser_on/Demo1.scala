4f621313f87501846c7af5ab143f39cbe5a723d5
twr143 <twr143@gmail.com>
2018-09-15 10:59:22 +0300

    deserialization test, 2

--- src/main/scala/ser_on/Demo1.scala
+++ src/main/scala/ser_on/Demo1.scala
@@ -1,36 +1,27 @@
 package ser_on
-
 import java.util.{Properties, UUID}
-
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema
+import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.types.Row
 import com.github.plokhotnyuk.jsoniter_scala.core._
 /**
   * Created by Ilya Volynin on 14.09.2018 at 18:26.
   */
 object Demo1 {
-  def main(args:Array[String]){
+  def main(args: Array[String]) {
     import Model._
-
-      val env = StreamExecutionEnvironment.getExecutionEnvironment
-      val properties = new Properties()
-      properties.setProperty("bootstrap.servers", "localhost:9092")
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val properties = new Properties()
+    properties.setProperty("bootstrap.servers", "localhost:9092")
     properties.setProperty("group.id", s"myflinkservice_${UUID.randomUUID}")
     properties.setProperty("auto.offset.reset", "earliest")
-      val kafkaConsumer011 = new FlinkKafkaConsumer011[String]("testT1", new SimpleStringSchema(), properties)
-      val stream: DataStream[String] = env.addSource[String](kafkaConsumer011)
-      val result: DataStream[Row] = stream.filter(_.nonEmpty).flatMap{ p =>
-        val obj = readFromArray(p.getBytes("UTF-8"))
-        obj match {
-          case o: SerializationBeanJsoniter => println("1")
-          case o: SerializationBeanJsoniter2 => println("2")
-          case o: SerializationBeanJsoniter3 => println("3")
-          case _ => println("illegal obj")
-        }
-        Seq(Row.of(obj))
-      }
-      result.print().setParallelism(1)
-      env.execute()
-    }}
+    val kafkaConsumer011 = new FlinkKafkaConsumer011[String]("testT1", new SimpleStringSchema(), properties)
+    val stream: DataStream[String] = env.addSource[String](kafkaConsumer011)
+    val result: DataStream[Row] = stream.filter(_.nonEmpty).flatMap { p =>
+      Seq(Row.of(readFromArray(p.getBytes("UTF-8"))))
+    }
+    result.print().setParallelism(1)
+    env.execute()
+  }
+}
